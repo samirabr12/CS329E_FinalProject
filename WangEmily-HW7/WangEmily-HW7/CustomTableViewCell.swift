4d2dbe2d5a89f12cc2f7abc20814a492fc5924a2
oneoddoddity <47764326+oneoddoddity@users.noreply.github.com>
2022-11-01 20:39:19 -0500

    Add files via upload

---
+++ WangEmily-HW7/WangEmily-HW7/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  WangEmily-HW7
+//
+//  Created by Emily Wang on 10/31/22.
+//  Project: WangEmily-HW7
+//  EID: ew6985
+//  Course: CS329E
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var eventLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+}
