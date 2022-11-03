8d418cb227ea1b1de0c0ac5ba89a15fbe3062667
oneoddoddity <47764326+oneoddoddity@users.noreply.github.com>
2022-11-03 00:43:43 -0500

    Add files via upload

---
+++ CS329E_FinalProject-updated-structure/CS329E_FinalProject-main/CS329E_FinalProject/calculatorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  calculatorViewController.swift
+//  CS329E_FinalProject
+//
+//  Created by Emily Wang on 11/3/22.
+//
+
+import UIKit
+
+class calculatorViewController: UIViewController {
+    var calcDict: [String: Int] = [:]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    //when pressing button, check if any of the fields are empty/nil. if they are, display error. else, calculate
+    func calculate() {
+
+    }
+
+}
