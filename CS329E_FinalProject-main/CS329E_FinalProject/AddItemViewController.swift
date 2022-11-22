b0117dd97f5d85ec9be8d4e956e022bc6879b8d1
oneoddoddity <47764326+oneoddoddity@users.noreply.github.com>
2022-11-21 21:59:01 -0600

    Add files via upload

---
+++ CS329E_FinalProject-main/CS329E_FinalProject/AddItemViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddItemViewController.swift
+//  CS329E_FinalProject
+//
+//  Created by Emily on 11/21/22.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController {
+
+    @IBOutlet weak var itemSegCtrl: UISegmentedControl!
+    @IBOutlet weak var newItemView: UIView!
+    @IBOutlet weak var existItemView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func switchItem(_ sender: Any) {
+        switch itemSegCtrl.selectedSegmentIndex {
+        case 0:
+            UIView.animate(withDuration: 0.2, animations: { //want existing item view to be first since as item database grows, more likely to want existing items
+                self.existItemView.alpha = 1
+                self.newItemView.alpha = 0
+            })
+        case 1:
+            UIView.animate(withDuration: 0.2, animations: {
+                self.existItemView.alpha = 0
+                self.newItemView.alpha = 1
+                
+            })
+        default:
+            break //what does this do exactly?
+        }
+    }
+    
+}
